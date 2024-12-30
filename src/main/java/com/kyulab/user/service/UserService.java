314f96a249f726aa98d164b9f11622f8ec155308
Choikyudong <slgmgm@naver.com>
2024-12-30 21:49:13 +0900

    feat: grpc 기능 추가
    board <--> user grpc 테스트 코드 추가

--- src/main/java/com/kyulab/user/service/UserService.java
+++ src/main/java/com/kyulab/user/service/UserService.java
@@ -1,16 +1,22 @@
 package com.kyulab.user.service;
 
+import com.kyulab.HelloReply;
+import com.kyulab.HelloRequest;
+import com.kyulab.MyServiceGrpc;
 import com.kyulab.user.dto.User;
 import com.kyulab.user.repository.UserRepository;
+import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@GrpcService
 @RequiredArgsConstructor
-public class UserService {
+public class UserService extends MyServiceGrpc.MyServiceImplBase {
 
 	private final UserRepository userRepository;
 
@@ -38,4 +44,12 @@ public class UserService {
 		userRepository.deleteUserByUserName(userName);
 	}
 
+	@Override
+	public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+		String msg = "grpc 테스트용 입니당";
+		System.out.println("난 실행이 되었지롱?");
+		HelloReply reply = HelloReply.newBuilder().setMessage(msg).build();
+		responseObserver.onNext(reply);
+		responseObserver.onCompleted();
+	}
 }
