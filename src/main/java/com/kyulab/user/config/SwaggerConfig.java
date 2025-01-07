4baa77b956be4bfed309d3b57e322412f62cced5
Choikyudong <slgmgm@naver.com>
2025-01-08 00:35:30 +0900

    refactor: swagger jwt 토큰인증 기능 추가
    가독성을 위해 Info 분리
    헤더에 AUTHORIZATION의 값을 넣을 수 있도록 컴포넌트와 보안 추가
    
    스웨거로 테스트를 원할 경우 Authorize 버튼을 통해 토큰값을 입력

--- src/main/java/com/kyulab/user/config/SwaggerConfig.java
+++ src/main/java/com/kyulab/user/config/SwaggerConfig.java
@@ -1,20 +1,36 @@
 package com.kyulab.user.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
-	
+
 	@Bean
 	public OpenAPI apiDocDec() {
-		return new OpenAPI()
-				.info(new Info()
+		Info info = new Info()
 					.title("springdoc-openapi")
-					.version("0.0.1")
-					.description("여기는 user임"));
+					.version("0.0.2")
+					.description("user-app");
+
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION);
+		Components jwtTestComp = new Components() // 액세스 토큰인증 기능 추가
+								.addSecuritySchemes(HttpHeaders.AUTHORIZATION, new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")
+								.in(SecurityScheme.In.HEADER)
+								.name(HttpHeaders.AUTHORIZATION));
+		return new OpenAPI()
+				.info(info)
+				.components(jwtTestComp)
+				.addSecurityItem(securityRequirement);
 	}
 	
 }
