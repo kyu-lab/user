c6086b0f1c23d731b6e3a2509f0e563790a53653
Choikyudong <slgmgm@naver.com>
2025-01-08 00:37:22 +0900

    refactor: redis 캐싱 설정 및 스프링부트 옵션 추가
    jwt의 리프레쉬 토큰을 레디스에서 관리하기 위해 몇가지 옵션 추가
    스프링부트에서 캐시를 활용하여 레디스와 통신하기 위해 옵션 추가
    jpa로 데이터 업데이트시 업데이트 시간 자동 어노테이션 추가

--- src/main/java/com/kyulab/user/UserApplication.java
+++ src/main/java/com/kyulab/user/UserApplication.java
@@ -2,8 +2,12 @@ package com.kyulab.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA 엔티티 생성 및 수정시간 자동 기록
+@EnableCaching // 스프링 부트에 캐싱기능 필요 전달
 public class UserApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/kyulab/user/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.kyulab.user.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	@Bean
+	public CacheManager rcm(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+				.entryTtl(Duration.ofMinutes(10L)); // 캐시 항목의 TTL을 10분으로 설정
+
+		return RedisCacheManager
+				.RedisCacheManagerBuilder
+				.fromConnectionFactory(redisConnectionFactory)
+				.cacheDefaults(redisCacheConfiguration)
+				.build();
+	}
+
+}
